4dbb622cb04e89bdf24b837fe68092f5902c654e
Shabanov Mikhail <sensdoo.dev@gmail.com>
2025-02-09 09:52:52 +0300

    Added route and handler in controller for getting recipe information from API on server side

--- server/src/controllers/recipeController.js
+++ server/src/controllers/recipeController.js
@@ -24,7 +24,36 @@ module.exports = class RecipeController {
       if (!response) {
         return res
           .status(400)
-          .json(formatResponse(400, 'Ошибка получения данных из "poonacular.com"'));
+          .json(formatResponse(400, 'Ошибка получения данных из стороннего API'));
+      }
+
+      res.status(200).json(formatResponse(200, 'OK', response.data));
+    } catch (error) {
+      res
+        .status(500)
+        .json(formatResponse(500, 'Unxpected server error', null, error.message));
+    }
+  }
+
+  static async getRecipeInformationById(req, res) {
+    try {
+      const { recipeId } = req.params;
+
+      if (!recipeId) {
+        return res.status(400).json(formatResponse(400, 'Не передан ID рецепта"'));
+      }
+
+      const response = await axios.get(
+        `${process.env.API_RECIPES}/${recipeId}/information?apiKey=${process.env.API_KEY}`,
+        {
+          headers: { 'Content-Type': 'application/json' },
+        },
+      );
+
+      if (!response) {
+        return res
+          .status(400)
+          .json(formatResponse(400, 'Ошибка получения данных из стороннего API'));
       }
 
       res.status(200).json(formatResponse(200, 'OK', response.data));
--- server/src/routes/api/recipes.routes.js
+++ server/src/routes/api/recipes.routes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const RecipeController = require('../../controllers/recipeController');
 
 router.get('/complexSearch', RecipeController.complexSearch);
+router.get('/recipeInformation/:recipeId', RecipeController.getRecipeInformationById);
 
 module.exports = router;
